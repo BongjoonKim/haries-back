a00aff6645c56cef1a942096fd835d588a73ac78
bongjoonkim <bongjoonkim96@gmail.com>
2023-01-02 18:04:14 +0900

    백엔드 개발 시작

---
+++ src/main/java/com/example/hariesbackend/HariesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hariesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HariesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HariesBackendApplication.class, args);
+	}
+
+}
