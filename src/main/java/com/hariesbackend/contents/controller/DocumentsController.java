9e857781bd8656af1754447598691e2c24a7d47f
bongjoonkim <bongjoonkim96@gmail.com>
2023-04-04 23:57:58 +0900

    [공통] 프론트 백엔드 글 목록 API 연결 완료

--- src/main/java/com/hariesbackend/contents/controller/DocumentsController.java
+++ src/main/java/com/hariesbackend/contents/controller/DocumentsController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/documents")
+@RequestMapping("example/documents")
 @Slf4j
 public class DocumentsController {
     @Autowired
