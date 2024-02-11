096a34def81058acfd0680d95d7437f530ba39bf
bongjoonkim <bongjoonkim96@gmail.com>
2024-02-12 01:34:22 +0900

    GPT 질문, 응답 요약 구성 고민 중

--- src/main/java/com/hariesbackend/chatgpt/service/serviceImpl/GPTServiceImpl.java
+++ src/main/java/com/hariesbackend/chatgpt/service/serviceImpl/GPTServiceImpl.java
@@ -52,25 +52,78 @@ public class GPTServiceImpl implements GPTService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer " + token);
 
-        List<MessagesHistory> summaryMessage = messageHistoryRepository.findByChannelIdAndUserId(channelId, "Summary");
-        if (summaryMessage.size() > 0) {
+        GPTRequestDTO requestDTO = null;
+
+
+        List<MessagesHistory> summarySystemMessage = messageHistoryRepository.findByChannelIdAndUserId(channelId, "SummarySystem");
+        List<MessagesHistory> summaryUserMessage = messageHistoryRepository.findByChannelIdAndUserId(channelId, "SummaryUser");
+        if (summarySystemMessage.size() > 0 || summaryUserMessage.size() > 0) {
             GPTMessageDTO messageDTO = new GPTMessageDTO("user", question);
-            GPTMessageDTO messageSummary = new GPTMessageDTO("system", summaryMessage.get(0).getContent());
+            GPTMessageDTO messageSummarySystem = new GPTMessageDTO("asistant", summarySystemMessage.get(0).getContent());
+            GPTMessageDTO messageSummaryUser = new GPTMessageDTO("user", summaryUserMessage.get(0).getContent());
+
             List<GPTMessageDTO> messageList = new ArrayList<>();
 
+            messageList.add(messageDTO);
+            messageList.add(messageSummarySystem);
+            messageList.add(messageSummaryUser);
 
-//            GPTRequestDTO requestDTO = new GPTRequestDTO("gpt-4-1106-preview", 1.0, false, messageDTO));
+            requestDTO = new GPTRequestDTO("gpt-4-1106-preview", 1.0, false, messageList);
 
         } else {
             GPTMessageDTO messageDTO = new GPTMessageDTO("user", question);
-            GPTRequestDTO requestDTO = new GPTRequestDTO("gpt-4-1106-preview", 1.0, false, Collections.singletonList(messageDTO));
-
-            HttpEntity<GPTRequestDTO> httpEntity = new HttpEntity<>(requestDTO, headers);
-            RestTemplate restTemplate = new RestTemplate();
-
-            ResponseEntity<GPTResponseDTO> response = restTemplate.postForEntity(uri, httpEntity, GPTResponseDTO.class);
-
-            return response.getBody().getChoices().get(0).getMessage().getContent();
+            requestDTO = new GPTRequestDTO("gpt-4-1106-preview", 1.0, false, Collections.singletonList(messageDTO));
         }
+        HttpEntity<GPTRequestDTO> httpEntity = new HttpEntity<>(requestDTO, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<GPTResponseDTO> response = restTemplate.postForEntity(uri, httpEntity, GPTResponseDTO.class);
+
+        //////////
+        GPTMessageDTO newMessageSummaryUser = null;
+
+        // 질문 요약 정리
+        if (summarySystemMessage.size() > 0) {
+
+            GPTMessageDTO answerDTO = new GPTMessageDTO("assistant", response.getBody().getChoices().get(0).getMessage().getContent());
+            GPTMessageDTO newMessageSummarySystem= newMessageSummarySystem = new GPTMessageDTO("assistant", summarySystemMessage.get(0).getContent()); // 과거 답 요약
+            GPTMessageDTO systemSummaryRequest = new GPTMessageDTO("user","대답을 요약해줘"); // 과거 답 요약
+            
+            List<GPTMessageDTO> summaryAnswerList = new ArrayList<>();
+            summaryAnswerList.add(answerDTO);
+            summaryAnswerList.add(newMessageSummarySystem);
+            summaryAnswerList.add(systemSummaryRequest);
+
+            GPTRequestDTO summaryAnswerDTO = new GPTRequestDTO("gpt-4-1106-preview", 1.0, false, summaryAnswerList);
+            HttpEntity<GPTRequestDTO> httpEntityOfAnswer = new HttpEntity<>(summaryAnswerDTO, headers);
+            ResponseEntity<GPTResponseDTO> responseAnswer = restTemplate.postForEntity(uri, httpEntityOfAnswer, GPTResponseDTO.class);
+
+        } else {    // sumarry가 없을 경우 첫 번째 질문이 summary가 된다
+
+        }
+
+        if (summaryUserMessage.size() > 0) {
+            // 응답 요약 정리
+            GPTMessageDTO questionDTO = new GPTMessageDTO("user", question); // 과거 답 요약
+            newMessageSummaryUser = new GPTMessageDTO("user", summaryUserMessage.get(0).getContent()); // 과거 답 요약
+            GPTMessageDTO systemSummaryquestion = new GPTMessageDTO("user","질문을 요약해줘"); // 과거 답 요약
+
+            List<GPTMessageDTO> summaryQuestionList = new ArrayList<>();
+            summaryQuestionList.add(newMessageSummaryUser);
+            summaryQuestionList.add(questionDTO);
+            summaryQuestionList.add(systemSummaryquestion);
+
+            GPTRequestDTO summaryQuestionDTO = new GPTRequestDTO("gpt-4-1106-preview", 1.0, false, summaryQuestionList);
+            HttpEntity<GPTRequestDTO> httpEntityOfQuestion = new HttpEntity<>(summaryQuestionDTO, headers);
+
+            ResponseEntity<GPTResponseDTO> responseQuestion = restTemplate.postForEntity(uri, httpEntityOfQuestion, GPTResponseDTO.class);
+        } else {    // sumarry가 없을 경우 첫 번째 질문이 summary가 된다
+
+        }
+
+
+
+
+        return response.getBody().getChoices().get(0).getMessage().getContent();
     }
 }
