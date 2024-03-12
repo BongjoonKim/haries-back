b21f5bf24cd1bf961b1c14dfabb04373d693a222
bongjoonkim <bongjoonkim96@gmail.com>
2024-03-13 01:31:53 +0900

    Dalle 백엔드 개발 완료

--- src/main/java/com/hariesbackend/dalle/service/serviceImpl/DalleServiceImpl.java
+++ src/main/java/com/hariesbackend/dalle/service/serviceImpl/DalleServiceImpl.java
@@ -1,11 +1,20 @@
 package com.hariesbackend.dalle.service.serviceImpl;
 
+import com.hariesbackend.dalle.dto.DalleReqDTO;
 import com.hariesbackend.dalle.dto.DalleResDTO;
 import com.hariesbackend.dalle.service.DalleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Slf4j
 @Service
@@ -21,6 +30,17 @@ public class DalleServiceImpl implements DalleService {
 
     @Override
     public DalleResDTO DalleAnswer(String channelId, String question) {
-        return null;
+        URI uri = UriComponentsBuilder.fromUriString(dalleUri).build().toUri();
+        HttpHeaders headers = new org.springframework.http.HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + token);
+
+        DalleReqDTO dalleReqDTO = new DalleReqDTO("dall-e-3", question, 1, "1024x1024");
+
+        HttpEntity<DalleReqDTO> httpEntity = new HttpEntity<>(dalleReqDTO, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<DalleResDTO> response = restTemplate.postForEntity(uri, httpEntity, DalleResDTO.class);
+        return response.getBody();
     }
 }
